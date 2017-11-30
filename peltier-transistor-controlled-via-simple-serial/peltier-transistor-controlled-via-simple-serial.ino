53cd64752942f78400d80f8be3d3debbb471d487
molecule <molecule@berkeley.edu>
2017-11-30 13:59:00 -0800

    add stuff to control peltier element w transistor

---
+++ peltier-transistor-controlled-via-simple-serial/peltier-transistor-controlled-via-simple-serial.ino
@@ -0,0 +1,44 @@
+/*
+Serial code based off of original work
+ by Tom Igoe
+ created 13 Apr 2012
+ 
+ modified 14 Mar 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+ */
+
+const int controlPin = 9;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(controlPin, OUTPUT);
+  Serial.println("simple serial........for breakfast!");
+}
+
+void loop() {
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+    int val = Serial.parseInt();
+    analogWrite(controlPin, val);
+    delay(50);
+  
+    // look for the newline. That's the end of your
+    // sentence:
+    if (Serial.read() == '\n') {
+      Serial.print("val: ");
+      Serial.println(val);
+    }
+  }
+}
+
+
+
+
+
+
+
+
